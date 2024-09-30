2250903b84dc5c4835f3f4735a6ec75c7da76c7a
Ali <86645460+AliMoradpour@users.noreply.github.com>
2024-09-30 08:18:15 +0330

    intro to next.js image comp

--- frontend/src/app/blogs/_components/PostList.tsx
+++ frontend/src/app/blogs/_components/PostList.tsx
@@ -1,5 +1,6 @@
-const PostList = async () => {
+import Image from "next/image";
 
+const PostList = async () => {
   const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/post/list`);
   const {
     data: { posts },
@@ -7,8 +8,17 @@ const PostList = async () => {
 
   type PostType = {
     title: string;
+    coverImage: string;
   };
-  return posts.map((post: PostType) => <div>{post.title}</div>);
+  return posts.length > 0 ? (
+    <div className="grid grid-cols-12 gap-8">
+      {posts.map((post: PostType) => (
+        <div className="col-span-12 sm:col-span-6 lg:col-span-4 border border-secondary-100 p-2 rounded-lg">
+          <Image src={post.coverImage} alt="cover image" width={400} height={400}/>
+        </div>
+      ))}
+    </div>
+  ) : null;
 };
 
 export default PostList;
