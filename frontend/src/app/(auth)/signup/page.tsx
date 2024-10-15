1a35872149383f5d968f159c7b152f65c7742018
Ali <86645460+AliMoradpour@users.noreply.github.com>
2024-10-15 16:03:19 +0330

    create signin and signup routes

---
+++ frontend/src/app/(auth)/signup/page.tsx
@@ -0,0 +1,23 @@
+import TextField from "@/ui/TextField";
+
+export const metadata = {
+  title: "ثبت نام",
+};
+
+const Signup = () => {
+  return <div>
+    <h1>ثبت نام</h1>
+    <form>
+        <TextField 
+        name="name"
+        label="نام و نام خانوادگی"
+        // value={}
+        // onChange={}
+        isRequired
+        
+        />
+    </form>
+  </div>;
+};
+
+export default Signup;
