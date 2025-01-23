0cda2506c3f00c2cdc119386a8c5b9e2165b6a84
Ali <86645460+AliMoradpour@users.noreply.github.com>
2025-01-23 08:20:35 +0330

    adding suspense for fetching

--- frontend/src/app/(dashboard)/profile/page.tsx
+++ frontend/src/app/(dashboard)/profile/page.tsx
@@ -1,18 +1,20 @@
-import { fetchCardData } from "@/services/data";
-import { Card } from "./_components/Cards";
-import PostsTable from "./posts/_/components/PostsTable";
+import { Suspense } from "react";
+import CardsWrapper from "./_components/CardsWrapper";
+import SpinnerMini from "@/ui/SpinnerMini";
+import LatestPost from "./_components/LatestPost";
 
 const Profile = async () => {
-  const { numberOfComments, numberOfPosts, numberOfUsers } = await fetchCardData();
   return (
     <div>
-      <div className="grid gap-6 md:grid-cols-3 mb-8">
-       <Card title="کاربران" value={numberOfUsers} type="users"/>
-       <Card title="پست ها" value={numberOfPosts} type="posts"/>
-       <Card title="نظرات" value={numberOfComments} type="comments"/>
-      </div>
+      <h1 className="text-xl mb-8 text-secondary-700">داشبورد</h1>
+      <Suspense fallback={<SpinnerMini />}>
+        <CardsWrapper />
+      </Suspense>
 
-      <PostsTable query="sort=latest&limit=5"/>
+      <h2 className="text-xl mb-4 text-secondary-600">آخرین پست ها</h2>
+      <Suspense fallback={<SpinnerMini />}>
+        <LatestPost />
+      </Suspense>
     </div>
   );
 };
--- frontend/src/app/(dashboard)/profile/posts/page.tsx
+++ frontend/src/app/(dashboard)/profile/posts/page.tsx
@@ -1,11 +1,15 @@
-import PostsTable from "./_/components/PostsTable"
+import { Suspense } from "react";
+import PostsTable from "./_/components/PostsTable";
+import SpinnerMini from "@/ui/SpinnerMini";
 
 const page = () => {
   return (
     <div>
-        <PostsTable />
+      <Suspense fallback={<SpinnerMini />}>
+        <PostsTable query="" />
+      </Suspense>
     </div>
-  )
-}
+  );
+};
 
-export default page
+export default page;
