739c65e5e1efce032ce303c671f6b2f76dcd6ce3
Ali <86645460+AliMoradpour@users.noreply.github.com>
2025-01-23 08:30:45 +0330

    search post

--- frontend/src/app/(dashboard)/profile/posts/page.tsx
+++ frontend/src/app/(dashboard)/profile/posts/page.tsx
@@ -1,12 +1,21 @@
 import { Suspense } from "react";
 import PostsTable from "./_/components/PostsTable";
 import SpinnerMini from "@/ui/SpinnerMini";
+import Search from "@/ui/Search";
+import { CreatePost } from "./_/components/Buttons";
+import queryString from "query-string";
 
-const page = () => {
+const page = (searchParams : any) => {
+  const query = queryString.stringify(searchParams);
   return (
     <div>
-      <Suspense fallback={<SpinnerMini />}>
-        <PostsTable query="" />
+      <div className="grid grid-cols-1 lg:grid-cols-3 gap-8 text-secondary-700 mb-12 items-center">
+        <h1 className="font-bold text-xl">لیست پست ها</h1>
+        <Search />
+        <CreatePost />
+      </div>
+      <Suspense fallback={<SpinnerMini />} key={query}>
+        <PostsTable query={query} />
       </Suspense>
     </div>
   );
